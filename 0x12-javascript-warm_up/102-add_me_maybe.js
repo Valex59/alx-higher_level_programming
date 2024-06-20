7c9b336396475b3fbaafa79659f8e185d6a60d13
Valex59 <valex4real5929@gmail.com>
2024-06-20 22:50:03 +0000

    A function that executes x times a function.

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 
-exports.callMeMoby = function (x, theFunction) {
-  let i = 0;
-
-  while (i < x) {
-    theFunction();
-    i++;
-  }
-};
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
+};;
