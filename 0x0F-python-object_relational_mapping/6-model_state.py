ecb9e3b8ca9521377fae5ac7e9f36c9181065efd
Valex59 <valex4real5929@gmail.com>
2024-06-29 04:46:44 +0000

    a script that prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,13 +1,45 @@
 #!/usr/bin/python3
 """
-This script defines a State class and
-a Base class to work with MySQLAlchemy ORM.
+Module 6-model_state
+Defines a State class that links to the MySQL table 'states'
+and creates the table in the database using SQLAlchemy.
 """
 import sys
-from model_state import Base, State
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import (create_engine)
+Base = declarative_base()
+
+class State(Base):
+    """
+    State class
+    Inherits from Base
+    Links to the MySQL table 'states'
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+def main():
+    """
+    Main function that connects to the MySQL server and creates
+    the 'states' table.
+    """
+    if len(sys.argv) != 4:
+        print("Usage: ./6-model_state.py <username> <password> <database_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}',
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    main()
+
